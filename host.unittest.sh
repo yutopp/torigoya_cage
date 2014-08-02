f42d6a317bf93f7861291569b42c50e3defe7050
yutopp <yutopp@gmail.com>
2014-08-02 19:39:32 +0900

    remove test name

--- host.unittest.sh
+++ host.unittest.sh
@@ -4,4 +4,4 @@ echo "Torigoya test: build packages..."
 ./host.build.sh || exit -1
 
 echo "Torigoya test: run unittest..."
-sudo GOPATH=`pwd` go test -v yutopp/cage -test.run="TestTicketRepeat"
+sudo GOPATH=`pwd` go test -v yutopp/cage
