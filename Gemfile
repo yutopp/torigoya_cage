1f9e75cae3eddccd3bffb89785656b0520c27a31
yutopp <yutopp@gmail.com>
2015-07-28 00:26:16 +0900

    remove unused files

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'torigoya_kit'
-gem 'colorize'
--- host.systemtest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$1" != "skip" ]; then
-    echo "Torigoya test: build packages..."
-    ./host.build.sh || exit -1
-fi
-
-echo "Torigoya test: run system test..."
-sudo bundle exec ruby test/system_test.rb
